78bdf7dc20f957d237544dbf7746ccd4ceb7de11
SametSisartenep <rodrigosloop@gmail.com>
2015-08-04 01:23:14 +0200

    Working on the test 'Framework'.

--- tests/test.c
+++ tests/test.c
@@ -9,25 +9,21 @@
 #include "../colors.h"
 
 int main ( int argc, char **argv ) {
-  printf("Starting Test \"Framework\"...\n\n");
 
-  printf("\x1b[1;31mRED\x1b[0m color test: ");
-  printf(strcmp(red("Some text"), "\x1b[0;31mSome text\x1b[0m") == 0 ? "\x1b[0;32mOK\x1b[0m\n" : "\x1b[0;31mBAD :'(\x1b[0m\n");
+  const char *text = "Some Text Here";
 
-  printf("\x1b[1;32mGREEN\x1b[0m color test: ");
-  printf(strcmp(green("Some text"), "\x1b[0;32mSome text\x1b[0m") == 0 ? "\x1b[0;32mOK\x1b[0m\n" : "\x1b[0;31mBAD :'(\x1b[0m\n");
-
-  printf("\x1b[1;33mYELLOW\x1b[0m color test: ");
-  printf(strcmp(yellow("Some text"), "\x1b[0;33mSome text\x1b[0m") == 0 ? "\x1b[0;32mOK\x1b[0m\n" : "\x1b[0;31mBAD :'(\x1b[0m\n");
-
-  printf("\x1b[1;34mBLUE\x1b[0m color test: ");
-  printf(strcmp(blue("Some text"), "\x1b[0;34mSome text\x1b[0m") == 0 ? "\x1b[0;32mOK\x1b[0m\n" : "\x1b[0;31mBAD :'(\x1b[0m\n");
-
-  printf("\x1b[1;35mMAGENTA\x1b[0m color test: ");
-  printf(strcmp(magenta("Some text"), "\x1b[0;35mSome text\x1b[0m") == 0 ? "\x1b[0;32mOK\x1b[0m\n" : "\x1b[0;31mBAD :'(\x1b[0m\n");
-
-  printf("\x1b[1;36mCYAN\x1b[0m color test: ");
-  printf(strcmp(cyan("Some text"), "\x1b[0;36mSome text\x1b[0m") == 0 ? "\x1b[0;32mOK\x1b[0m\n" : "\x1b[0;31mBAD :'(\x1b[0m\n");
+  red(text);
+  printf("\n");
+  green(text);
+  printf("\n");
+  yellow(text);
+  printf("\n");
+  blue(text);
+  printf("\n");
+  magenta(text);
+  printf("\n");
+  cyan(text);
+  printf("\n");
 
   printf("\n");
 
